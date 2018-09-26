7f3ee64e17588e6c08b851202298c90f3f9b6e50
teamlead11 <automationlead11@gmail.com>
2018-09-26 10:13:18 +0530

    initial commit

---
+++ src/testpack/BaseClass.java
@@ -0,0 +1,16 @@
+package testpack;
+
+public class BaseClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		BaseClass obj = new BaseClass();
+		obj.palndromByVignesh();
+	}
+
+	public void palndromByVignesh() {
+
+	}
+
+}
