ff55580861f09d30ab4a08306e93dbde59ad399f
Ramyadhiya <ramya14.email@gmail.com>
2018-09-26 12:26:24 +0530

    Add one reverse string method

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -1,16 +1,37 @@
 package testpack;
 
+import java.util.Scanner;
+
 public class BaseClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 		BaseClass obj = new BaseClass();
 		obj.palndromByVignesh();
+		obj.palindromeByRamya();
 	}
 
 	public void palndromByVignesh() {
 
 	}
+	public void palindromeByRamya()
+	{   int len;
+		String str,rev="";
+		@SuppressWarnings("resource")
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter your string");
+		str=sc.nextLine();
+		len=str.length();
+		for(int i=len-1;i>=0;i--)
+		{
+			rev=rev+str.charAt(i);
+		}
+		if(str.equals(rev))
+		{
+			System.out.println("This's polindrome string");
+		}else{
+			System.out.println("This's not polindrome string");
+		}
+		}
 
 }
