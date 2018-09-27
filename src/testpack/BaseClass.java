304f4a4fc6a6e95febbcd1f481899bc77fb4f578
vickyinfo <rajvicky.43@gmail.com>
2018-09-27 08:33:53 +0530

    final change branch vicky

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -9,98 +9,42 @@ public class BaseClass {
 		BaseClass obj = new BaseClass();
 		obj.palndromByVignesh();
 		obj.palindromeByRamya();
-		
-		
-		
+
 		obj.palindromeByVicky();
 	}
 
 	public void palndromByVignesh() {
 
 	}
-	public void palindromeByRamya()
-	{   int len;
-		String str,rev="";
+
+	public void palindromeByRamya() {
+		int len;
+		String str, rev = "";
 		@SuppressWarnings("resource")
-		Scanner sc=new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter your string");
-		str=sc.nextLine();
-		len=str.length();
-		for(int i=len-1;i>=0;i--)
-		{
-			rev=rev+str.charAt(i);
+		str = sc.nextLine();
+		len = str.length();
+		for (int i = len - 1; i >= 0; i--) {
+			rev = rev + str.charAt(i);
 		}
-		if(str.equals(rev))
-		{
+		if (str.equals(rev)) {
 			System.out.println("This's polindrome string");
-		}else{
+		} else {
 			System.out.println("This's not polindrome string");
 		}
-		}
+	}
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	public void palindromeByVicky() {
 		String v = "madam";
 		String reverse = "";
-	      for (int i =v.length()-1; i >=0; i--) {
+		for (int i = v.length() - 1; i >= 0; i--) {
 			reverse += v.charAt(i);
-			if(reverse.equalsIgnoreCase(v)) {
-				System.out.println(v+ "palindrome");
-			}else {
-				System.out.println(v+ "not a palindrome");
-			
+			if (reverse.equalsIgnoreCase(v)) {
+				System.out.println(v + "palindrome");
+			} else {
+				System.out.println(v + "not a palindrome");
+
 			}
 		}
 
