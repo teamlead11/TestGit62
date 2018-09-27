0f3689d75a63c9d64e0ed37fd2c1706a1de7efdb
vickyinfo <rajvicky.43@gmail.com>
2018-09-27 08:05:00 +0530

    add new commit by vickybranch

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -9,6 +9,10 @@ public class BaseClass {
 		BaseClass obj = new BaseClass();
 		obj.palndromByVignesh();
 		obj.palindromeByRamya();
+		
+		
+		
+		obj.palindromeByVicky();
 	}
 
 	public void palndromByVignesh() {
@@ -34,4 +38,71 @@ public class BaseClass {
 		}
 		}
 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public void palindromeByVicky() {
+		String v = "madam";
+		String reverse = "";
+	      for (int i =v.length()-1; i >=0; i--) {
+			reverse += v.charAt(i);
+			if(reverse.equalsIgnoreCase(v)) {
+				System.out.println(v+ "palindrome");
+			}else {
+				System.out.println(v+ "not a palindrome");
+			
+			}
+		}
+
+	}
 }
