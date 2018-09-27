c818a71d0ca1dd73a50ce483c7f15cc1c0490046
sivaranjini2511 <sivaranjinicse@gmail.com>
2018-09-27 12:29:18 +0530

    branch1 modify

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -8,9 +8,9 @@ public class BaseClass {
 		// TODO Auto-generated method stub
 		BaseClass obj = new BaseClass();
 		obj.palndromByVignesh();
-    obj.palindromeByRamya();
+		obj.palindromeByRamya();
 		obj.sivasankarPalindrome();
-       obj.ajithkumarPalindrome();
+		obj.ajithkumarPalindrome();
 		obj.liviyaPalindrome();
 
 	}
@@ -19,74 +19,83 @@ public class BaseClass {
 
 	}
 
-
-	public void palindromeByRamya()
-	{   int len;
-		String str,rev="";
+	public void palindromeByRamya() {
+		int len;
+		String str, rev = "";
 		@SuppressWarnings("resource")
-		Scanner sc=new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter your string");
-		str=sc.nextLine();
-		len=str.length();
-		for(int i=len-1;i>=0;i--)
-		{
-			rev=rev+str.charAt(i);
+		str = sc.nextLine();
+		len = str.length();
+		for (int i = len - 1; i >= 0; i--) {
+			rev = rev + str.charAt(i);
 		}
-		if(str.equals(rev))
-		{
+		if (str.equals(rev)) {
 			System.out.println("This's polindrome string");
-		}else{
+		} else {
 			System.out.println("This's not polindrome string");
 		}
-		}
-  	public void sivasankarPalindrome() {
+	}
+
+	public void sivasankarPalindrome() {
 		String s = "amma";
 		String r = null;
 		System.out.println(s);
-		int l=s.length();
-		for (int i = l-1; i >=0; i--) {
+		int l = s.length();
+		for (int i = l - 1; i >= 0; i--) {
 			System.out.println(i);
-			r=r+s.charAt(l);
-		
+			r = r + s.charAt(l);
+
 		}
-		if(s.equals(r)){
-			System.out.println(r +"is Palindrome");
-			
+		if (s.equals(r)) {
+			System.out.println(r + "is Palindrome");
+
 		}
 
 	}
 
-  	public void ajithkumarPalindrome() {
-      String s = "MALAYALAM";
-      String reverse = "";
-      for (int i =s.length()-1; i >=0; i--) {
-		reverse += s.charAt(i);
-		if(reverse.equalsIgnoreCase(s)) {
-			System.out.println(s+ "palindrome");
-		}else {
-			System.out.println(s+ "not a palindrome");
-		
+	public void ajithkumarPalindrome() {
+		String s = "MALAYALAM";
+		String reverse = "";
+		for (int i = s.length() - 1; i >= 0; i--) {
+			reverse += s.charAt(i);
+			if (reverse.equalsIgnoreCase(s)) {
+				System.out.println(s + "palindrome");
+			} else {
+				System.out.println(s + "not a palindrome");
+
+			}
 		}
+
 	}
 
+	public void liviyaPalindrome() {
+		int r, sum = 0, temp;
+		int n = 454;
+		temp = n;
+		while (n > 0) {
+			r = n % 10;
+			sum = (sum * 10) + r;
+			n = n / 10;
+		}
+		if (temp == sum)
+			System.out.println("palindrome number ");
+		else
+			System.out.println("not palindrome");
+	}
 
+	public void sivabranch1() {
+		// TODO Auto-generated method stub
+		int r, sum = 0, temp;
+		int n = 454;
+		temp = n;
+		while (n > 0) {
+			r = n % 10;
+			sum = (sum * 10) + r;
+			n = n / 10;
+		}
+		if (temp == sum)
+			System.out.println("palindrome number ");
 
-
-}
-
-
-public void liviyaPalindrome() {
-int r,sum=0,temp;    
-		  int n=454; 
-		   temp=n;    
-		  while(n>0){    
-		   r=n%10;    
-		   sum=(sum*10)+r;    
-		   n=n/10;    
-		  }    
-		  if(temp==sum)    
-		   System.out.println("palindrome number ");    
-		  else    
-		   System.out.println("not palindrome");    
-		}  
+	}
 }
