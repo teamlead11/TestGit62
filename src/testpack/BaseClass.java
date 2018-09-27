eb0cd7e965f552a45d307ce1e7091e96523d79ce
joy1523 <joybenezer@gmail.com>
2018-09-27 10:54:55 +0530

    added

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -13,6 +13,7 @@ public class BaseClass {
        obj.ajithkumarPalindrome();
 		obj.liviyaPalindrome();
 		obj.nisha();
+		obj.stingpaljoy();
 
 	}
 
@@ -105,4 +106,27 @@ public class BaseClass {
   				  else    
   				   System.out.println("not palindrome");    
   				}  
+
+
+
+	public void stingpaljoy() {
+		String em1,rev="";
+		System.out.println("Enter any String");
+		Scanner em=new Scanner(System.in);
+		em1=em.nextLine();
+		int l=em1.length();
+		System.out.println(em1);
+		for(int i=l-1;i>=0;i--)
+		{
+			rev=rev + em1.charAt(i);
+		}
+		if(em1.equals(rev))
+		{
+		System.out.println("palindrome");
+		}
+		else
+		{
+			System.out.println("not palindrome");
+		}
+	}
 }
