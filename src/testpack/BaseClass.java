64a58bdf76d6c31d3c4dec2d90692c7125d03129
sumathi2105 <sumathisomu2105@gmail.com>
2018-09-29 09:35:53 +0530

    updated again

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -120,21 +120,6 @@ public class BaseClass {
 			System.out.println("not palindrome");
 		}
 	}
-
-	public void vicky() {
-		int r, sum = 0, temp;
-		int n = 454;
-		temp = n;
-		while (n > 0) {
-			r = n % 10;
-			sum = (sum * 10) + r;
-			n = n / 10;
-		}
-		if (temp == sum)
-			System.out.println("palindrome number ");
-		else
-			System.out.println("not palindrome");
-	}
 	public void polindromeBySumathi() 
 	{
 		int r, sum = 0, temp;		
@@ -151,6 +136,22 @@ public class BaseClass {
 			System.out.println("not palindrome");
 	
 }
+
+	public void vicky() {
+		int r, sum = 0, temp;
+		int n = 454;
+		temp = n;
+		while (n > 0) {
+			r = n % 10;
+			sum = (sum * 10) + r;
+			n = n / 10;
+		}
+		if (temp == sum)
+			System.out.println("palindrome number ");
+		else
+			System.out.println("not palindrome");
+	}
+	
 }
 
 
