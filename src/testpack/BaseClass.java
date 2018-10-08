3712f553d6bff334bcb0965aec2df7d05e0ecc5f
Preethiakshara <preethi94akshara18@gmail.com>
2018-10-08 10:34:16 +0530

    palindrome program

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -151,7 +151,26 @@ public class BaseClass {
 		else
 			System.out.println("not palindrome");
 	}
-	
+public void palindromeByAkshara() {
+	String str,rev="";
+	@SuppressWarnings("resource")
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter your string");
+	str=sc.nextLine();
+	for(int i=str.length()-1;i>=0;i--)
+	{
+		rev=rev+str.charAt(i);
+	}
+	if(str.equals(rev))
+	{
+		System.out.println("This's polindrome string");
+	} else {
+		System.out.println("This's not polindrome string");
+	}
+
+
+}	
 }
+
 
 
