329492185eec0d00e07186c715c4db90e93a682e
suseendran93 <ssndrn@gmail.com>
2018-10-09 06:54:40 +0530

    Cucumber practice project

---
+++ Features/GuruLogin.feature
@@ -0,0 +1,9 @@
+@tag
+Feature: To test login functionality
+
+@tag1
+Scenario: To login with valid credentials
+
+Given The user is in Guru login page
+When The user enters username & password
+Then The user should be logged in
---
+++ src/main/java/org/cucumber/runners/TestRunner.java
@@ -0,0 +1,17 @@
+package org.cucumber.runners;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "Features")
+public class TestRunner {
+	
+	
+
+
+
+}
